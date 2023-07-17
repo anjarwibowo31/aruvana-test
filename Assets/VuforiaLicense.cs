57d97f1ac1937dff60dcc8e8dfc85a6748dd8a72
anjarwibowo31 <anjar31wibowo@gmail.com>
2023-07-17 21:56:10 +0700

    a bit more

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWZ3TFZ0SC8vLy8vQUFBQm1RNHV1YVp0SzB6MnNWMGo1TkpkUGpJZ0xlK0dvUXg4RFVPaVdVbUFXV3lJTzNRcnQ1R3h4TFFDMGZDdnphdFFhRTdtZGphZ0RrTFFiWmZYNXB3M0F3WlpBRnphRU5hMDZra2RMV2MzV2hyOTRSY0xGNkVIL0NHYWZyam9VRjZLSGJFaFZ2aWxSbHh0NXlKM3R2ZmxvSWlrd1lYRFR3K2d1aHJzdk93TkhkSGdXczRFS0JDM0R4RFBKM3VxQWkrODFYQWI5bEpUYTRjUEVvdWZ6cDBzUzd6ZTRTR0RrcXlqMXhjN3V5b25KZ0pBTmlHTXk0d0plYmgxY280cGYwQ3NFNDRXQldoREduMDdHY0RoalI5OE5MaytXaVpocnBHZlNkR1BFUStHa1YyZjhHbUlKbHN4b0hmNStoMGlPbk1ESGlRM1kzSEgyUDVOQmxhcmdzcUprSjh5K096dnYxSUlpaTdKUWZBUTc3MlE=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
